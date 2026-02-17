a7ba223e870b3a8ae52324c427c6987a89f511fc
Mike <mmdelez@gmail.com>
2026-02-17 14:23:51 +0100

    init commit

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { useTheme } from "@react-navigation/native";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  const { colors } = useTheme();
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: colors.background
+      }}
+    >
+      <Text style={{ color: colors.text }}>Welcome to the app!</Text>
+    </View>
+  );
+}
